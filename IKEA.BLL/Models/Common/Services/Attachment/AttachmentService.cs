fd5c1713ede99c8e822ec06a5d67d5c1c2f161d6
Aya Ali <yoyoali920@gmail.com>
2024-09-28 05:58:02 +0300

    Part 04 Attachment Service

---
+++ IKEA.BLL/Models/Common/Services/Attachment/AttachmentService.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IKEA.BLL.Models.Common.Services.Attachment
+{
+    internal class AttachmentService : IAttachmentService
+    {
+        private List<string> _AllowedExtensions = new() { ".png", ".jpg", ".jpeg" };
+        private const int _AllowedMaxSize = 2097152;
+
+        public string? Upload(IFormFile file, string FolderName)
+        {
+            var extension = Path.GetExtension(file.FileName);
+
+            if (!_AllowedExtensions.Contains(extension))
+                return null;
+            if(file.Length > _AllowedMaxSize) 
+                return null;
+
+            var FolderPath = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot\\Files", FolderName);
+            if(!Directory.Exists(FolderPath))
+                Directory.CreateDirectory(FolderPath);
+            var FileName=$"{Guid.NewGuid()}{extension}";
+            var FilePath =Path.Combine(FolderPath, FileName);
+            var FileStream = new FileStream(FilePath, FileMode.Create);
+            file.CopyTo(FileStream);
+
+            return FileName;
+        }
+        public bool Delete(string FilePath)
+        {
+            if (File.Exists(FilePath))
+            {
+                File.Delete(FilePath);
+                return true;
+            }
+            else
+                return false;
+          
+        }
+
+      
+    }
+}
---
+++ IKEA.BLL/Models/Common/Services/Attachment/IAttachmentService.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IKEA.BLL.Models.Common.Services.Attachment
+{
+    internal interface IAttachmentService
+    {
+        string? Upload(IFormFile file ,string FolderName);
+        bool Delete(string FilePath);
+
+
+    }
+}
