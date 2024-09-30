ed4131640c1a78c4fda6d023e6c6b7c3e38e02a7
Aya Ali <yoyoali920@gmail.com>
2024-09-30 08:08:26 +0300

    Part 04 Account Controller - SignIn

--- IKEA.PL/Controllers/AccountController.cs
+++ IKEA.PL/Controllers/AccountController.cs
@@ -7,27 +7,27 @@ namespace IKEA.PL.Controllers
 {
     public class AccountController : Controller
     {
-		private readonly UserManager<ApplicationUser> _userManager;
-		private readonly SignInManager<ApplicationUser> _signInManager;
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly SignInManager<ApplicationUser> _signInManager;
 
-		public AccountController(UserManager<ApplicationUser>  userManager,SignInManager<ApplicationUser> signInManager)
+        public AccountController(UserManager<ApplicationUser> userManager, SignInManager<ApplicationUser> signInManager)
         {
-			_userManager = userManager;
-			_signInManager = signInManager;
-		}
+            _userManager = userManager;
+            _signInManager = signInManager;
+        }
         [HttpGet]
-       public IActionResult SignUp ()
+        public IActionResult SignUp()
         {
             return View();
         }
 
-		[HttpPost]
-        public async Task< IActionResult> SignUp(SignUpViewModel model)
-		{
-			if(!ModelState.IsValid) 
+        [HttpPost]
+        public async Task<IActionResult> SignUp(SignUpViewModel model)
+        {
+            if (!ModelState.IsValid)
                 return BadRequest();
 
-            var User =await _userManager.FindByNameAsync(model.UserName);
+            var User = await _userManager.FindByNameAsync(model.UserName);
             if (User == null)
             {
                 User = new ApplicationUser()
@@ -37,7 +37,7 @@ namespace IKEA.PL.Controllers
                     Email = model.Email,
                     IsAgree = model.IsAgree,
                     UserName = model.UserName,
-                    
+
                 };
                 var result = await _userManager.CreateAsync(User, model.Password);
                 if (result.Succeeded)
@@ -48,7 +48,7 @@ namespace IKEA.PL.Controllers
                 {
                     foreach (var Error in result.Errors)
                     {
-                        ModelState.AddModelError(string.Empty ,Error.Description);
+                        ModelState.AddModelError(string.Empty, Error.Description);
                     }
                 }
             }
@@ -58,12 +58,46 @@ namespace IKEA.PL.Controllers
             }
 
             return View(model);
-		}
+        }
 
-		[HttpGet]
-		public IActionResult SignIn()
-		{
-			return View();
+        [HttpGet]
+        public IActionResult SignIn()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> SignIn(SignInViewModel model)
+        {
+			if (!ModelState.IsValid)
+				return BadRequest();
+			var User = await _userManager.FindByNameAsync(model.Email);
+            if(User is { })
+            {
+                var Flag = await _userManager.CheckPasswordAsync(User, model.Password);
+                if(Flag)
+                {
+                    var result = await _signInManager.PasswordSignInAsync(User ,model.Password,model.RememberMe,true);
+
+					if (result.IsNotAllowed)
+
+						ModelState.AddModelError(string.Empty, "Your account is not confirmed yet ");
+
+					if (result.IsLockedOut)
+
+						ModelState.AddModelError(string.Empty, "Your account is locked ");
+
+					if (result.Succeeded)
+
+						return RedirectToAction(nameof(HomeController.Index), "Home");
+				}
+
+               
+            }
+			ModelState.AddModelError(nameof(SignUpViewModel.UserName), " Failed To Login ");
+
+			return View(model);
+
 		}
 	}
 }
